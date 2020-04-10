f2c0b0afba1f6508d47b08da4a4bcee4681926d9
AlexVipond <alex.r.vipond@gmail.com>
2020-04-10 16:30:38 -0600

    More accurate and explicit testing

--- tests/function.test.js
+++ tests/function.test.js
@@ -1,29 +1,37 @@
 import test  from 'ava'
-import defaultConfig from './defaultConfig.stub'
-import linearNumeric from '../src/tailwind'
-
-test('returns the linear numeric config object', t => {
-  const value = linearNumeric(),
-        withoutMaxWidth = ({ maxWidth, ...rest }) => rest // Can't compare identities of anonymous functions, so gotta remove it
-
-  t.deepEqual(withoutMaxWidth(value), defaultConfig)
-})
+import linearNumeric from '../src'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import expectedKeys from './expectedKeys.stub.js'
 
 test('respects the "only" option as an array', t => {
-  const value = linearNumeric({ only: ['spacing'] }).spacing
+  const theme = linearNumeric({ only: ['spacing'] }),
+        keys = Object.keys(theme),
+        expected = ['spacing']
 
-  t.deepEqual(value, defaultConfig.spacing)
+  t.deepEqual(keys, expected)
 })
 
 test('respects the "only" option as a string', t => {
-  const value = linearNumeric({ only: 'spacing' })
+  const theme = linearNumeric({ only: 'spacing' }),
+        keys = Object.keys(theme)
 
-  t.deepEqual(value, defaultConfig.spacing)
+  t.deepEqual(keys, expectedKeys.spacing)
 })
 
 test('respects the "increment" option', t => {
-  const value = linearNumeric({ increment: 42 }),
-        suffixes = Object.keys(value.spacing)
+  const theme = linearNumeric({ increment: 42 }),
+        keys = Object.keys(theme.spacing)
 
-  t.assert(suffixes.includes('42'))
+  t.assert(keys.includes('42'))
+})
+
+const incrementables = Object.keys(expectedKeys)
+incrementables.forEach(incrementable => {
+  test(`properly increments ${incrementable}`, t => {
+    const defaultTheme = resolveConfig({ theme: linearNumeric() }),
+          theme = incrementable === 'colors' ? defaultTheme.theme.colors.blue : defaultTheme.theme[incrementable],
+          keys = Object.keys(theme)
+  
+    t.deepEqual(keys, expectedKeys[incrementable])
+  })
 })
