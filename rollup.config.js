34da5c43f6abc3ad7d7db2bf73cb8bb31eeb3845
AlexVipond <alex.r.vipond@gmail.com>
2020-11-04 23:06:33 -0600

    upgrades babel toolchain and properly lists dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 
 const external = [
         'tailwindcss/defaultConfig',
         'tailwindcss/resolveConfig',
         '@baleada/tailwind-theme-utils',
+        /@babel\/runtime/,
       ],
       plugins = [
         babel({
           exclude: 'node_modules',
+          babelHelpers: 'runtime',
         }),
         resolve(),
       ]
